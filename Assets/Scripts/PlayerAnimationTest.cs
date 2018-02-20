513e763728f792ee93f631f0003d905169fe6d7b
Kylian <development@goudcode.nl>
2018-02-20 20:46:21 +0100

    Added a simple animation test scene

---
+++ Assets/Scripts/PlayerAnimationTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationTest : MonoBehaviour
+{
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine("switchAnimator");
+    }
+
+    IEnumerator switchAnimator()
+    {
+        while (true)
+        {
+            bool currentState = animator.GetBool("IsRunning");
+            animator.SetBool("IsRunning", !currentState);
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
