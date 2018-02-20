06a1d7f65c422a8994269c5754b0e01b08249b53
Kylian <development@goudcode.nl>
2018-02-20 22:14:59 +0100

    Renamed armature, tweaked the animation test code

--- Assets/Scripts/PlayerAnimationTest.cs
+++ Assets/Scripts/PlayerAnimationTest.cs
@@ -10,16 +10,17 @@ public class PlayerAnimationTest : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        StartCoroutine("SwitchAnimator");
+        StartCoroutine(SwitchAnimator());
     }
 
     IEnumerator SwitchAnimator()
     {
+        var timeout = new WaitForSeconds(5);
         while (true)
         {
             bool currentState = animator.GetBool("IsRunning");
             animator.SetBool("IsRunning", !currentState);
-            yield return new WaitForSeconds(5);
+            yield return timeout;
         }
     }
 }
