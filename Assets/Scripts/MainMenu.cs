2386caa041366f3350d497ccbb9eaa96b4baba94
TheGoldenDragon <ritseart@hotmail.nl>
2018-02-21 21:44:43 +0100

    Changed the menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-    public GameObject createRoomPanel;
+    public GameObject mainMenuPanel;
+
+    public GameObject newGameMenuPanel;
+    public GameObject singlePlayerPanel;
+    public GameObject hostGamePanel;
+
+    public GameObject serverBrowserMenuPanel;
     public GameObject serverBrowserPanel;
-    public GameObject optionsPanel;
+
+    public GameObject optionsMenuPanel;
+
+    public GameObject exitGamePanel;
+
+
+
 
     private void Start()
     {
@@ -20,34 +32,111 @@ public class MainMenu : MonoBehaviour {
         SceneManager.LoadScene("Game");
     }
 
-    public void ShowCreateRoomPanel()
+    public void ShowMainMenuPanel()
     {
-        createRoomPanel.SetActive(true);
+        mainMenuPanel.SetActive(true);
     }
 
-    public void HideCeateRoomPanel()
+    public void HideMainMenuPanel()
     {
-        createRoomPanel.SetActive(false);
+        mainMenuPanel.SetActive(false);
     }
 
-    public void ShowServerBrowser()
+    public void ShowNewGameMenuPanel()
+    {
+        newGameMenuPanel.SetActive(true);
+        ShowSinglePlayerPanel();
+        HideMainMenuPanel();
+    }
+
+    public void HideNewGameMenuPanel()
+    {
+        newGameMenuPanel.SetActive(false);
+    }
+
+    public void ShowServerBrowserMenuPanel()
+    {
+        serverBrowserMenuPanel.SetActive(true);
+        ShowServerBrowserPanel();
+        HideMainMenuPanel();
+    }
+
+    public void HideServerBrowserMenuPanel()
+    {
+        serverBrowserMenuPanel.SetActive(false);
+    }
+
+    public void ShowOptionsMenuPanel()
+    {
+        optionsMenuPanel.SetActive(true);
+    }
+
+    public void HideOptionsMenuPanel()
+    {
+        optionsMenuPanel.SetActive(false);
+    }
+
+    public void ShowSinglePlayerPanel()
+    {
+        singlePlayerPanel.SetActive(true);
+        HideHostGamePanel();
+    }
+
+    public void HideSinglePlayerPanel()
+    {
+        singlePlayerPanel.SetActive(false);
+    }
+
+    public void ShowHostGamePanel()
+    {
+        hostGamePanel.SetActive(true);
+        HideSinglePlayerPanel();
+    }
+
+    public void HideHostGamePanel()
+    {
+        hostGamePanel.SetActive(false);
+    }
+
+    public void ShowServerBrowserPanel()
     {
         serverBrowserPanel.SetActive(true);
     }
 
-    public void HideServerBrowser()
+    public void HideServerBrowserPanel()
     {
         serverBrowserPanel.SetActive(false);
     }
     
-    public void ShowOptions()
+    public void ShowOptionsMenu()
     {
-        optionsPanel.SetActive(true);
+        optionsMenuPanel.SetActive(true);
     }
 
-    public void HideOptions()
+    public void HideOptionsMenu()
     {
-        optionsPanel.SetActive(false);
+        optionsMenuPanel.SetActive(false);
+    }
+   
+    public void ShowExitGamePanel()
+    {
+        exitGamePanel.SetActive(true);
+    }
+
+    public void HideExitGamePanel()
+    {
+        exitGamePanel.SetActive(false);
+    }
+
+    public void HideAllPanelsExceptMain()
+    {
+        mainMenuPanel.SetActive(true);
+        newGameMenuPanel.SetActive(false);
+        singlePlayerPanel.SetActive(false);
+        hostGamePanel.SetActive(false);
+        serverBrowserMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(false);
+        exitGamePanel.SetActive(false);
     }
 
     public void ExitGame()
