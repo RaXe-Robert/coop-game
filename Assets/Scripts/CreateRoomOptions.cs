2eea7efc314ab7d624b612d700e022e40f844695
TheGoldenDragon <ritseart@hotmail.nl>
2018-02-26 20:11:54 +0100

    In game menu implementation

--- Assets/Scripts/CreateRoomOptions.cs
+++ Assets/Scripts/CreateRoomOptions.cs
@@ -8,42 +8,40 @@ public class CreateRoomOptions : MonoBehaviour
     [SerializeField] private Text roomNameText;
     [SerializeField] private Dropdown playerCountDropdown;
     [SerializeField] private Dropdown roomVisibilityDropdown;
+    [SerializeField] private Dropdown objectDropdown;
 
     #region Public Methods
-
     public void OnClick_CreateRoom()
     {
         RoomOptions roomOptions = new RoomOptions()
         {
-
-            IsVisible = roomVisibilityDropdown.options[roomVisibilityDropdown.value].text == "PUBLIC" ? true : false,
+            CleanupCacheOnLeave = objectDropdown.options[objectDropdown.value].text == "SERVER BOUND" ? false : true,
+            IsVisible = roomVisibilityDropdown.options[roomVisibilityDropdown.value].text == "PUBLIC" ? true : false,            
             MaxPlayers = Convert.ToByte(playerCountDropdown.options[playerCountDropdown.value].text.Substring(0, 1)),
-            IsOpen = true
+            IsOpen = true            
         };
 
         if (PhotonNetwork.CreateRoom(roomNameText.text, roomOptions, TypedLobby.Default))
         {
-            print("create room succesfully sent.");
+            print("Succesfully requested a room");
         }
         else
         {
-            print("create room failed to send");
+            print("Failed to send a room request!");
         }
     }
-
     #endregion //Public Methods
 
     #region Photon Callbacks
 
     private void OnPhotonCreateRoomFailed(object[] codeAndMessage)
     {
-        print("create room failed: " + codeAndMessage[1]);
+        print("Failed to create a room: " + codeAndMessage[1]);
     }
 
     private void OnCreatedRoom()
     {
-        print("Room created succesfully.");
-
+        print("Succesfully created a room");
         PhotonNetwork.LoadLevel("Game");
     }
 
