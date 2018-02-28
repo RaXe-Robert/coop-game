d88249f05f6ebc913d6f7903078fb89afdac52b2
RaXe-Robert <robertbijl@live.nl>
2018-02-28 12:57:56 +0100

    Changed to menu load delay to 0 for the unity_editor

--- Assets/Scripts/GameInitialization.cs
+++ Assets/Scripts/GameInitialization.cs
@@ -7,7 +7,11 @@ public class GameInitialization : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
+#if UNITY_EDITOR
+        StartCoroutine(DelayedMainMenuLoad(0f));
+#else
         StartCoroutine(DelayedMainMenuLoad(1.5f));
+#endif
     }
 
     private IEnumerator DelayedMainMenuLoad(float seconds)
