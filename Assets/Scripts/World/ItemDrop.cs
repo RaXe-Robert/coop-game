c822b5d5af52bc182517540bb717a76d7434f506
TheGoldenDragon <ritseart@hotmail.nl>
2018-02-28 01:36:08 +0100

    Added scripts for objects

---
+++ Assets/Scripts/World/ItemDrop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour {
+
+    [SerializeField] private ObjectHealth objectHealth;
+    [SerializeField] private List<GameObject> itemObject;
+    [SerializeField] private List<int> itemCount;
+    [SerializeField] private int minRadius;
+    [SerializeField] private int maxRadius;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (objectHealth.getBroken())
+        {
+            for(int x=0; x < itemObject.Count; x++)
+            {
+                for (int y = 0; y < itemCount[x]; y++)
+                {
+                    spawnObject(itemObject[x]);
+                    destroy();
+                }
+            }
+        }
+	}
+
+    void spawnObject(GameObject go)
+    {
+        Vector3 position = this.gameObject.transform.position;
+        Quaternion rotation = this.gameObject.transform.rotation;
+        rotation.x = 90;
+        position.x = Random.Range(minRadius, maxRadius);
+        position.z = Random.Range(minRadius, maxRadius);
+        Instantiate(go, position, rotation);
+    }
+
+    void destroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/World/ObjectHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHealth : MonoBehaviour {
+
+    [SerializeField] private int health;
+    [SerializeField] private bool broken = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0)
+        {
+            broken = true;
+        }
+    }
+
+    public bool getBroken()
+    {
+        return broken;
+    }
+
+    void OnMouseDown()
+    {
+        health -= 100;
+        Debug.Log(broken);
+        Debug.Log(health);
+    }
+}
