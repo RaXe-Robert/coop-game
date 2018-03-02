599299fd7450f3578f6c3ded03abdb346655c6a2
RaXe-Robert <robertbijl@live.nl>
2018-03-02 11:23:42 +0100

    Fixed the player object not being removed if a player left the room

---
+++ Assets/Scripts/Utility/DestroyOnOwnerLeave.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnOwnerLeave : Photon.MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        if (photonView != null)
+        {
+            if (photonView.owner == null)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            Debug.LogError("DestroyOnOwnerLeave must be attached to a photonView");
+            this.enabled = false;
+        }
+    }
+}
