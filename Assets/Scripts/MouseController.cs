f3c321f325f1af2801156b9ece034f0699ffd8c2
Kylian <development@goudcode.nl>
2018-03-04 17:16:22 +0100

    Fixed networking bug for mouse controller

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -7,19 +7,21 @@ public class MouseController : MonoBehaviour
 
     private Ray ray;
     private RaycastHit hit;
+    private bool isMine;
 
     private void Start()
     {
         playerCamera = GetComponentInChildren<Camera>();
+        isMine = GetComponent<PhotonView>().isMine;
     }
 
     private void Update()
     {
-        if (!Input.GetMouseButtonDown(0))
+        if (!isMine || !Input.GetMouseButtonDown(0))
             return;
 
         ray = playerCamera.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
             var interactable = hit.transform.GetComponentInChildren<IInteractable>();
             if (interactable != null && interactable.IsInteractable())
