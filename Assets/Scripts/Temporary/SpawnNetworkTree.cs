3d75a39932ea39bcdd8837b72d1c7d62f40157cc
TheGoldenDragon <ritseart@hotmail.nl>
2018-03-11 02:19:59 +0100

    Changed prefab to match photon

--- Assets/Scripts/Temporary/SpawnNetworkTree.cs
+++ Assets/Scripts/Temporary/SpawnNetworkTree.cs
@@ -9,10 +9,13 @@ public class SpawnNetworkTree : MonoBehaviour {
 	void Start () {
         if (PhotonNetwork.isMasterClient)
         {
-            for (int i = 0; i < 1000; i += 10)
+            for (int i = -100; i < 100; i += 20)
             {
-                Vector3 position = new Vector3(Random.Range(i, -100), 0f, Random.Range(-100, i));
-                PhotonNetwork.InstantiateSceneObject(go.name, position, Quaternion.Euler(0, Random.Range(0, 180), 0), 0, null);
+                for (int z = 100; z > -100; z -= 20)
+                {
+                    Vector3 position = new Vector3(Random.Range(i, z), 0f, Random.Range(i, z));
+                    PhotonNetwork.InstantiateSceneObject(go.name, position, Quaternion.Euler(0, Random.Range(0, 180), 0), 0, null);
+                }
             }
         }
     }
