a541caf0e170cddff3ccfb82dd354936981f8633
TheGoldenDragon <ritseart@hotmail.nl>
2018-03-16 16:13:01 +0100

    - Implemented Carl Carl will follow the player around when he gets too close. Carl will walk to a random point on the map when nobody is around. Carl will think for a bit when he lost the player out of his sight. This system uses the animator as being the Statemachine for an npc

---
+++ Assets/Scripts/NPC/CarlNPC.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarlNPC : MonoBehaviour {
+
+    private Animator animator;
+        
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        animator.SetFloat("Distance", Vector3.Distance(transform.position, PlayerNetwork.PlayerObject.transform.position));	
+	}
+}
---
+++ Assets/Scripts/NPC/Patrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : NPCBaseFSM {
+    Vector3 Waypoint;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+    }   
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Vector3.Distance(Waypoint, NPC.transform.position) < accuracy)
+        {
+            Waypoint = createWaypoint();
+        }
+
+        var direction = Waypoint - NPC.transform.position;
+        NPC.transform.rotation = Quaternion.Slerp(NPC.transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+        NPC.transform.Translate(0, 0, Time.deltaTime * speed);
+
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    Vector3 createWaypoint()
+    {
+        return NPC.transform.position + new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+    }
+
+   
+}
