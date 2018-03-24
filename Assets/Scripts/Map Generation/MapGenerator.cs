970633b706ce01ad1c99c1d43172807fe849ba70
Kylian <development@goudcode.nl>
2018-03-24 20:16:10 +0100

    Basic perlin noise generation

--- Assets/Scripts/Map Generation/MapGenerator.cs
+++ Assets/Scripts/Map Generation/MapGenerator.cs
@@ -10,14 +10,14 @@ public class Noise
         float[,] noiseMap = new float[width, height];
         scale = scale <= 0 ? 0.001f : scale;
 
-        for (int y = 0; y < height; ++y)
+        for (int y = 0; y < height; y++)
         {
-            for (int x = 0; x < width; ++x)
+            for (int x = 0; x < width; x++)
             {
                 float sampleX = x / scale;
-                float sampleY = x / scale;
+                float sampleY = y / scale;
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleX);
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
                 noiseMap[x, y] = perlinValue;
             }
         }
@@ -29,11 +29,15 @@ public class Noise
 public class MapGenerator : MonoBehaviour {
     public int width = 1024;
     public int height = 1024;
-    public float scale = 500f;
+    public float scale = .5F;
+    public bool autoUpdate = true;
 
     public void GenetateMap()
     {
+        var noiseMap = Noise.GenerateNoiseMap(width, height, scale);
 
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
     }
 
 
