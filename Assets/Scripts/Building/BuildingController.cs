c41a260e0fdc113cdd068a21e712ae2a654e34d6
RaXe-Robert <robertbijl@live.nl>
2018-03-28 18:51:21 +0200

    Improved the grid snapping of the BuildingController, added a special material for objects that are about to be build.

--- Assets/Scripts/Building/BuildingController.cs
+++ Assets/Scripts/Building/BuildingController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
 using UnityEngine;
 
@@ -16,6 +17,7 @@ public class BuildingController : Photon.MonoBehaviour
     [Range(1f, 10f)]
     [SerializeField] private float gridSpacing;
     [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Material buildingModeMaterial;
 
     private void Update()
     {
@@ -30,15 +32,21 @@ public class BuildingController : Photon.MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Activates the building mode, the buildable will be instantiated and follows the mouse.
+    /// </summary>
+    /// <param name="buildable"></param>
     public void ActivateBuildMode(Buildable buildable)
     {
         CancelBuildMode();
 
-        Object buildableResource = Resources.Load($"Buildables/{buildable.Name}");
+        UnityEngine.Object buildableResource = Resources.Load($"Buildables/{buildable.Name}");
         if (buildableResource != null)
         {
-            buildableToBuild = Instantiate(buildableResource, PlayerNetwork.PlayerObject.transform.position, Quaternion.identity) as GameObject;
             buildableData = buildable;
+
+            buildableToBuild = Instantiate(buildableResource, PlayerNetwork.PlayerObject.transform.position, Quaternion.identity) as GameObject;
+            ReplaceMaterials();
 
             StartCoroutine(FollowMouse());
         }
@@ -84,48 +92,41 @@ public class BuildingController : Photon.MonoBehaviour
 
                 if (snapToGrid)
                 {
+                    // Calculate X positon
                     float xSpacing = Mathf.Abs(position.x % gridSpacing);
+                    float xPosition = xSpacing >= (gridSpacing / 2) ? gridSpacing - xSpacing : xSpacing * -1;
+                    
+                    // Calculate Z position
                     float zSpacing = Mathf.Abs(position.z % gridSpacing);
+                    float zPosition = zSpacing >= (gridSpacing / 2) ? gridSpacing - zSpacing : zSpacing * -1;
 
-                    float xOffset = (xSpacing > gridSpacing / 2) ? (gridSpacing - xSpacing) : xSpacing;
-                    float zOffset = (zSpacing > gridSpacing / 2) ? (gridSpacing - zSpacing) : zSpacing;
-
-                    if (xSpacing > gridSpacing / 2)
-                    {
-                        if (position.x >= 0)
-                            position.x = position.x + xOffset;
-                        else
-                            position.x = position.x - xOffset;
-                    }
-                    else
-                    {
-                        if (position.x < 0)
-                            position.x = position.x + xOffset;
-                        else
-                            position.x = position.x - xOffset;
-                    }
-
-                    if (zSpacing > gridSpacing / 2)
-                    {
-                        if (position.z >= 0)
-                            position.z = position.z + zOffset;
-                        else
-                            position.z = position.z - zOffset;
-                    }
-                    else
-                    {
-                        if (position.z < 0)
-                            position.z = position.z + zOffset;
-                        else
-                            position.z = position.z - zOffset;
-                    }
-
-                    Debug.Log($"From: {hitInfo.point.x} | Remainder: {xSpacing} | To: {position.x}");
-                    Debug.Log($"From: {hitInfo.point.z} | Remainder: {zSpacing} | To: {position.z}");
+                    // Store the position
+                    position.x += position.x >= 0 ? xPosition : xPosition * -1;
+                    position.z += position.z >= 0 ? zPosition : zPosition * -1;
                 }
+                // Apply the position
                 buildableToBuild.transform.position = position;
             }
             yield return null;
         }
+    }
+
+    /// <summary>
+    /// Replaces all the materials present on the <see cref="buildableToBuild"/> with the <seealso cref="buildingModeMaterial"/>
+    /// </summary>
+    private void ReplaceMaterials()
+    {
+        if (buildableToBuild == null)
+            return;
+
+        Renderer renderer = buildableToBuild.GetComponent<Renderer>();
+        Material[] materials = new Material[renderer.materials.Length];
+
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i] = buildingModeMaterial;
+        }
+
+        renderer.materials = materials;
     }
 }
