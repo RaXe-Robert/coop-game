49bf6cb62f556dad00e64ad83f02cfa381d2c6b4
RaXe-Robert <robertbijl@live.nl>
2018-03-31 14:46:13 +0200

    Cleanup of some unused code.

--- Assets/Scripts/Building/BuildableWorldObject.cs
+++ Assets/Scripts/Building/BuildableWorldObject.cs
@@ -5,29 +5,20 @@ using System.Collections;
 public class BuildableWorldObject : Photon.MonoBehaviour, IInteractable
 {
     public Buildable buildable;
+
     public float interactDistance = 1f;
     
     #region IInteractable Implementation
 
     public bool IsInteractable()
     {
-        return true;
+        return interactDistance > 0f;
     }
 
     public void Interact(Vector3 invokerPosition)
     {
         //TODO: Needs interaction menu
         Debug.Log("Interacting");
-        
-        /*
-        if (Vector3.Distance(transform.position, invokerPosition) > interactDistance)
-        {
-            PlayerNetwork.PlayerObject.GetComponent<PlayerMovementController>().ItemToPickup = this.gameObject;
-            return;
-        }
-
-        PlayerNetwork.PlayerObject.GetComponent<Inventory>().AddItemById(buildable.Id, buildable.StackSize);
-        photonView.RPC(nameof(DestroyWorldObject), PhotonTargets.AllBuffered);*/
     }
 
     public string TooltipText()
