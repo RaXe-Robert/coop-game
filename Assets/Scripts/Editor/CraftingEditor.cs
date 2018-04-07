3d95264bdbcb27726aa4467f7d7b364a73ab2525
WouterHeslinga <Wouter_Heslinga@hotmail.com>
2018-04-07 19:57:55 +0200

    Making babysteps towards a crafting editor

---
+++ Assets/Scripts/Editor/CraftingEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+public class CraftingEditor : EditorWindow {
+
+    private CraftingList selectedCraftingList;
+    public List<CraftingRecipe> list;
+
+    private Rect listDisplay;
+    private Rect dataDisplay;
+    private ReorderableList itemList;
+
+    [MenuItem("Window/CraftingEditor")]
+    public static void ShowWindow()
+    {
+        GetWindow<CraftingEditor>("Crafting Editor");
+    }
+
+    private void OnGUI()
+    {
+        DrawListDisplay();
+        DrawDataDisplay();
+    }
+
+    private void DrawListDisplay()
+    {
+        listDisplay = new Rect(0, 0, position.width * 0.3f, position.height);
+        GUILayout.BeginArea(listDisplay);
+
+        //Input field for a crafting list
+        selectedCraftingList = (CraftingList)EditorGUILayout.ObjectField(selectedCraftingList, typeof(CraftingList), false);
+
+        if(selectedCraftingList != null)
+        {
+            //TODO: find a way to live update this list
+            if (GUILayout.Button("UpdateList"))
+            {
+                itemList = new ReorderableList(selectedCraftingList.recipes, typeof(CraftingRecipe),true,true,true,true);
+            }
+            itemList?.DoLayoutList();
+        }
+        GUILayout.EndArea();
+    }
+
+    private void DrawDataDisplay()
+    {
+        dataDisplay = new Rect(position.width * 0.3f, 0, position.width * 0.7f, position.height);
+        GUILayout.BeginArea(dataDisplay);
+        GUILayout.Label("crafting recipe options");
+        GUILayout.EndArea();
+    }
+}
