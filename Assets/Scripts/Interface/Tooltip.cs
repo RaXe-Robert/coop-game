6a3062129e4f586f1227b0f6a18c6a37784dedbd
TheGoldenDragon <ritseart@hotmail.nl>
2018-05-09 00:41:27 +0200

    - Added calculations for borders

--- Assets/Scripts/Interface/Tooltip.cs
+++ Assets/Scripts/Interface/Tooltip.cs
@@ -20,7 +20,82 @@ public class Tooltip : MonoBehaviour
     private void LateUpdate()
     {
         if (panel.activeSelf)
-            panel.transform.position = Input.mousePosition + new Vector3(0, 50, 0);
+        {
+            panel.transform.position = CalculatePosition();
+        }
+    }
+
+    private Vector3 CalculatePosition()
+    {
+        Vector3 position = new Vector3();
+        float defaultX = Input.mousePosition.x;
+        float defaultY = Input.mousePosition.y + 50F;
+        position.z = Input.mousePosition.z;
+
+        float distanceFromHorizontal = defaultX;
+        float distanceFromVertical = defaultY;
+
+        float preferredWidth = title.preferredWidth;
+        float preferredHeight = title.preferredHeight + description.preferredHeight + 50F;
+
+        if (title.preferredWidth < description.preferredWidth)
+            preferredWidth = description.preferredWidth;
+        
+        //Calculate distance from left or right border.
+        if (defaultX < Screen.width / 2)
+        {
+            distanceFromHorizontal = defaultX;
+        }
+        else if (defaultX > Screen.width / 2)
+        {
+            distanceFromHorizontal = Screen.width - defaultX;
+        }
+        
+        //Calculate the distance from upper or bottom border.
+        if (defaultY < Screen.height / 2)
+        {
+            distanceFromVertical = defaultY;
+        }
+        else if (defaultY > Screen.height / 2)
+        {
+            distanceFromVertical = Screen.height - defaultY;
+        }
+        
+
+        if (distanceFromHorizontal < preferredWidth)
+        {
+            if (defaultX < Screen.width / 2)
+            {
+                position.x = preferredWidth + distanceFromHorizontal;
+            }
+            else if (defaultX > Screen.width / 2)
+            {
+                position.x = Screen.width - preferredWidth - distanceFromHorizontal;
+            }
+        }
+        else
+        {
+            position.x = defaultX;
+        }
+
+        if (distanceFromVertical < preferredHeight)
+        {
+            if (defaultY < Screen.height / 2)
+            {
+                //Eventually replace defaultY with (preferredHeight + distanceFromVertical) to let the bottom border have extra offset.
+                position.y = defaultY;
+            }
+            else if (defaultY > Screen.height / 2)
+            {
+                position.y = Screen.height - preferredHeight - distanceFromVertical;
+            }
+        }
+        else
+        {
+            position.y = defaultY;
+        }
+        
+        return position;      
     }
 
     /// <summary>
