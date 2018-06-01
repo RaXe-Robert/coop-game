525468ff850b53f568e136c47e201e5e6f312729
TheGoldenDragon <ritseart@hotmail.nl>
2018-06-01 15:15:04 +0200

    Made chest buildable and interactable Added animationcontroller Animations do now work properly and can not be interrupted

---
+++ Assets/Scripts/Items/Buildables/DataClasses/Chest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Chest : BuildableWorldObject
+{
+    private Animator animator;
+    public bool IsOpened { get; private set; } = false;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    protected override UnityAction[] InitializeActions()
+    {
+        return new UnityAction[]
+        {
+            new UnityAction(OpenChest),
+            new UnityAction(CloseChest)
+        };
+    }
+
+    private void OpenChest()
+    {
+        Debug.Log("Opening");
+        IsOpened = true;
+        animator.SetBool("IsOpen", true);
+    }
+
+    private void CloseChest()
+    {
+        Debug.Log("Closing");
+        IsOpened = false;
+        animator.SetBool("IsOpen", false);
+    }
+
+   
+}
